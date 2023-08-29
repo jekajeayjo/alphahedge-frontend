62ceab0f521172ff88d0c15ece3777d0f4870290
forestvov1 <kukva2019@gmail.com>
2023-08-29 21:32:39 +1000

    fix tab body

--- src/components/personal/TabBody/ui/TabBody.tsx
+++ src/components/personal/TabBody/ui/TabBody.tsx
@@ -5,8 +5,6 @@ import { PackageList } from 'components/personal/PackageList'
 import { IncomeTableCarousel, Individual } from 'components/personal/Individual'
 import { AdminBack } from 'components/admins/AdminBack'
 
-import { useAuth } from 'hooks/useAuth'
-
 import { tabEnum } from '../model/TabBody.interface'
 
 import { TabBodyButtons } from './TabBodyButtons'
@@ -17,8 +15,6 @@ export const TabBody = (props: { adminEdit: boolean }) => {
   const { adminEdit } = props
 
   const [tab, setTab] = useState<tabEnum>(tabEnum.ACTIVE)
-
-  const { role } = useAuth()
 
   const onClickHandler = (type: tabEnum) => {
     setTab(type)
